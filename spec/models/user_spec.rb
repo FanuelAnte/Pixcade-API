1d4759819f5d09da5c14549f5a3fbf2f8c794284
alazarlemma02 <alazar03lemma@gmail.com>
2023-10-26 03:00:29 +0300

    added the user registration process

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  attribs = [
+    {first_name: %i[presence]},
+    {last_name: %i[presence]},
+    {user_name: %i[presence uniqueness]},
+    {email: %i[presence uniqueness]},
+    {date_of_birth: %i[presence]},
+    {user_type: %i[presence]}
+  ]
+  include_examples("model_shared_spec", :user, attribs)
+
+  it { is_expected.to(have_many(:reviews)) }
+  it { is_expected.to(have_many(:ratings)) }
+  it { is_expected.to(have_many(:games)) }
+  it { is_expected.to(have_many(:wishlist_items)) }
+
+  describe "profile Image" do
+    let(:user) { FactoryBot.create(:user) }
+
+    before do
+      image_path = Rails.root.join("spec", "fixtures", "profile_pic.jpg")
+      user.profile_image.attach(io: File.open(image_path), filename: "profile_image.jpg", content_type: "image/jpeg")
+    end
+
+    it "can have a profile image attached" do
+      expect(user.profile_image).to be_attached
+    end
+  end
 end
